11e53d223a4ed20ee5798a5767a7c3dcfb037c32
Bruno Sousa <brunosousa_24@hotmail.com>
2018-04-10 16:45:34 +0100

    bootstrap css|setup to highlight navbar

--- react-proj/src/Perfil.js
+++ react-proj/src/Perfil.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Grid , Row , Col , Clearfix } from 'react-bootstrap';
 
 class Perfil extends Component {
 
@@ -12,97 +11,92 @@ class Perfil extends Component {
   }
 
   render() {
+    document.getElementById("perfilNavBar").className="w3-white w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"
     return (
       <div className="Perfil">
-        <Grid>
-          <Row>
-            <Col xs={6}>
-              <h1>O Meu Perfil</h1>
-            </Col>
-          </Row>
-          <Row>
-          <div id="data">
-            <Col xs={12} sm={6}>
-              <Row>
-                <Col xs={6} sm={6}>
-                  Primeiro Nome
-                </Col>
-                <Col xs={6} sm={6}>
-                  Manuel
-                </Col>
-              </Row>
-              <Row>
-                <Col xs={6} sm={6}>
-                  Ultimo Nome
-                </Col>
-                <Col xs={6} sm={6}>
-                  Pereira
-                </Col>
-              </Row>
-              <Row>
-                <Col xs={6} sm={6}>
-                  Username
-                </Col>
-                <Col xs={6} sm={6}>
-                  manuel123
-                </Col>
-              </Row>
-              <Row>
-                <Col xs={6} sm={6}>
-                  Email
-                </Col>
-                <Col xs={6} sm={6}>
-                  manuel123@mail.com
-                </Col>
-              </Row>
-              <Row>
-                <Col xs={6} sm={6}>
-                  Telemóvel
-                </Col>
-                <Col xs={6} sm={6}>
-                  +351 909 090 901
-                </Col>
-              </Row>
-            </Col>
+        <div className="row">
+          <div className="col-sm-6">
+            <h1>O Meu Perfil</h1>
           </div>
-          <div id="money">
-            <Col xs={12} sm={5} smOffset={1} className="w3-border w3-margin-top">
-              <Row>
-                <Col xs={6} sm={8}>
-                  Saldo:
-                </Col>
-                <Col xs={6} sm={4}>
-                  1000€
-                </Col>
-              </Row>
-              <Row>
-                <Col xs={6} sm={8}>
-                  Total Investido:
-                </Col>
-                <Col xs={6} sm={4}>
-                  2000€
-                </Col>
-              </Row>
-              <Row>
-                <Col xs={6} sm={8}>
-                  Total de Ganho/Perda:
-                </Col>
-                <Col xs={6} sm={4}>
-                  +134.5€
-                </Col>
-              </Row>
-              <Row>
-                <Col xs={6} sm={8}>
-                  Equitity:
-                </Col>
-                <Col xs={6} sm={4}>
-                  3134.5€
-                </Col>
-              </Row>
-            </Col>
+        </div>
+        <div className="row">
+          <div className="col-sm-12 col-md-5 offset-md-1">
+            <div className="row">
+              <div className="col-sm-6 col-md-6">
+                Primeiro Nome:
+                </div>
+              <div className="col-sm-6 col-md-6">
+                Manuel
+                </div>
+            </div>
+            <div className="row">
+              <div className="col-sm-6 col-md-6">
+                Ultimo Nome:
+                </div>
+              <div className="col-sm-6 col-md-6">
+                Pereira
+                </div>
+            </div>
+            <div className="row">
+              <div className="col-sm-6 col-md-6">
+                Username:
+                </div>
+              <div className="col-sm-6 col-md-6">
+                manuel123
+                </div>
+            </div>
+            <div className="row">
+              <div className="col-sm-6 col-md-6">
+                Email:
+                </div>
+              <div className="col-sm-6 col-md-6">
+                manuel123@mail.com
+                </div>
+            </div>
+            <div className="row">
+              <div className="col-sm-6 col-md-6">
+                Telemóvel:
+                </div>
+              <div className="col-sm-6 col-md-6">
+                +351 909 090 901
+                </div>
+            </div>
           </div>
-          </Row>
-        </Grid>
+          <div className="col-sm-12 col-md-4 offset-md-1 w3-border w3-margin-top">
+            <div className="row">
+              <div className="col-sm-6 col-md-8">
+                Saldo:
+                </div>
+              <div className="col-sm-6 col-md-4">
+                1000€
+                </div>
+            </div>
+            <div className="row">
+              <div className="col-sm-6 col-md-8">
+                Total Investido:
+                </div>
+              <div className="col-sm-6 col-md-4">
+                2000€
+                </div>
+            </div>
+            <div className="row">
+              <div className="col-sm-6 col-md-8">
+                Total de Ganho/Perda:
+                </div>
+              <div className="col-sm-6 col-md-4">
+                +134.5€
+                </div>
+            </div>
+            <div className="row">
+              <div className="col-sm-6 col-md-8">
+                Equitity:
+                </div>
+              <div className="col-sm-6 col-md-4">
+                3134.5€
+                </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
