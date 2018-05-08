4486bb78c2f35111e982e4fca3bc6a7e5f9c60df
Bruno Sousa <brunosousa_24@hotmail.com>
2018-05-08 13:51:36 +0100

    Registo

--- react-proj/src/Registo.js
+++ react-proj/src/Registo.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Redirect , browserHistory , StaticRouter } from 'react-router';
+import { Redirect, browserHistory, StaticRouter } from 'react-router';
 import Portfolio from './Portfolio';
-
-
-
 
 class Registo extends Component {
 
@@ -12,45 +9,41 @@ class Registo extends Component {
         super(props);
         this.state = {
         }
-      }
+    }
 
+    render() {
+        return (
+            <div className="Registo">
+                <div class="w3-container w3-center  w3-padding-8">
+                    <form>
+                        <div class="w3-container w3-center w3-padding-8" >
+                            <p> Nome Completo </p>
+                            <input type="text" ref="nome" />
+                        </div>
+                        <div class="w3-container w3-center w3-padding-8" >
+                            <p> Username </p>
+                            <input type="text" ref="username" />
+                        </div>
+                        <div class="w3-container w3-center w3-padding-8">
+                            <p> Password </p>
+                            <input type="password" ref="password" />
+                            <p> Confirmar Password </p>
+                            <input type="password" ref="password-conf" />
+                        </div>
+                        <div class="row w3-center w3-padding-16 w3-padding-large">
+                            <div class="col-md-1 col-md-offset-5 col-xs-2 col-xs-offset-4">
+                                <Link to="/App"><button>Voltar</button></Link>
+                            </div>
+                            <div class="col-md-1 col-xs-2">
+                                <Link to="/App"><button>Confirmar</button></Link>
+                            </div>
+                        </div>
+                    </form>
+                </div>
 
-      
-
-
-  render() {
-    return (
-<div className="Registo">  
-    <div class="w3-container w3-center  w3-padding-8">        
-      <form> 
-        <div class="w3-container w3-center w3-padding-8" >
-            <p> Nome Completo </p>
-            <input type="text" ref="nome"/> 
-        </div>
-        <div class="w3-container w3-center w3-padding-8" >
-            <p> Username </p>
-            <input type="text" ref="username"/> 
-        </div>
-        <div class="w3-container w3-center w3-padding-8">
-                <p> Password </p>
-                <input type="password" ref="password"/>
-                <p> Confirmar Password </p>
-                <input type="password" ref="password-conf"/>
-        </div>
-        <div class="row w3-center w3-padding-16 w3-padding-large">
-            <div class="col-md-1 col-md-offset-5 col-xs-2 col-xs-offset-4">
-                <Link to="/App"><button>Voltar</button></Link>
             </div>
-            <div class="col-md-1 col-xs-2">
-                <Link to="/App"><button>Confirmar</button></Link>
-            </div>
-        </div>
-    </form>
-    </div>
-      
-</div>
-    );
-  }
+        );
+    }
 }
 
 
